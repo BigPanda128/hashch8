239041afbd85ba7e44ce9c89fe10418877a3171b
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-13 02:33:22 -0500

    added the HashTable to separatechain file to insure that the test code reads it without going through the load command

--- separatechain.js
+++ separatechain.js
@@ -1,4 +1,39 @@
-load("HashTable.js");
+//load("HashTable.js");
+
+function HashTable() {
+	 this.table = new Array(137);
+	  this.simpleHash = simpleHash;
+	   this.betterHash = betterHash;
+	    this.showDistro = showDistro;
+	     this.put = put;
+	      this.get = get;
+	      this.buildChains = buildChains;
+	      }
+	      
+	      function simpleHash(data) {
+	       var total = 0;
+	        for (var i = 0; i < data.length; ++i) {
+	         total += data.charCodeAt(i);
+	          }
+	           return total % this.table.length;
+	           }
+	      
+	           function betterHash(string) {
+	            var H = 31;
+	             var total = 0;
+	              for (var i = 0; i < string.length; ++i) {
+	               total += H * total + string.charCodeAt(i);
+	                }
+	                 total = total % this.table.length;
+	                  if (total < 0) {
+	                   total += this.table.length-1;
+	                    }
+	                     return parseInt(total);
+	                     }
+
+
+
+
 function buildChains() {
 	for (var i = 0; i < this.table.length; ++i) {
 		this.table[i] = [];
