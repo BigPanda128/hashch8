c170dae50e6371238828e68b43d5d7c006d4bc82
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-13 02:42:13 -0500

    test code for the chaining funtion

---
+++ test-8-5.js
@@ -0,0 +1,12 @@
+load("separatechain.js");
+var hTable = new HashTable();
+hTable.buildChains();
+var someNames = ["David", "Jennifer", "Donnie", "Raymond",
+     "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
+for (var i = 0; i < someNames.length; ++i) {
+	 hTable.put(someNames[i]);
+}
+for (var i = 0; i < someNames.length; ++i) {
+	 hTable.put(someNames[i]);
+}
+hTable.showDistro();
