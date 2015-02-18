26feaf62a4834cbf758fc04c550dab50d2e9721a
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-18 00:03:34 -0500

    finished commented up version

--- exercise1Ch8.js
+++ exercise1Ch8.js
@@ -4,10 +4,16 @@
 //Chapter 8 exercise 1
 //create a basic dictionary using linear probing to store the definitions.
 
+//gets access to linear probing
 load("linearprobing.js");
 
+//create a new hash table
 var words = new HashTable();
 
+//this doesnt have to be a function however i made it one
+//for cleanliness
+//it gets the list of words and definitions and separats them and stores
+//them into the hash table
 function getWords() {
 	var word = read("words.txt").split("\n");
 	for (var i = 0; i < word.length; ++i) {
@@ -19,8 +25,15 @@ function getWords() {
 	}
 }
 
+//calls the function so it does its process
 getWords();
+
+//displays the words so the user can choose one to type in
 words.showDistro();
+
+//lets user type in the word
 print("Pick a word to see the definition: ");
 var input = readline();
+
+//prints the users definition of the word they selescted
 print(words.get(input));
