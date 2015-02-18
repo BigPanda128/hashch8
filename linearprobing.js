3ccb196fa3d0c98d81c24874f29555d946cb5ed1
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-17 22:23:25 -0500

    corrected indentation

--- linearprobing.js
+++ linearprobing.js
@@ -4,51 +4,50 @@
 //chapter 8 linear probing
 
 function HashTable() {
-	 this.table = new Array(137);
-	  this.simpleHash = simpleHash;
-	   this.betterHash = betterHash;
-	    this.showDistro = showDistro;
-	     this.put = put;
-	      this.get = get;
-	      this.values = [];
-	      }
+	this.table = new Array(137);
+	this.simpleHash = simpleHash;
+	this.betterHash = betterHash;
+	this.showDistro = showDistro;
+	this.put = put;
+	this.get = get;
+	this.values = [];
+}
 	      
-	      function simpleHash(data) {
-	       var total = 0;
-	       for (var i = 0; i < data.length; ++i) {
-	         total += data.charCodeAt(i);
-	          }
-	           return total % this.table.length;
-	           }
+function simpleHash(data) {
+	var total = 0;
+	for (var i = 0; i < data.length; ++i) {
+		total += data.charCodeAt(i);
+	}
+	return total % this.table.length;
+}
 	      
-	           function betterHash(string) {
-	            var H = 31;
-	             var total = 0;
-	              for (var i = 0; i < string.length; ++i) {
-	               total += H * total + string.charCodeAt(i);
-	                }
-	                 total = total % this.table.length;
-	                  if (total < 0) {
-	                   total += this.table.length-1;
-	                    }
-	                     return parseInt(total);
-	                     }
+function betterHash(string) {
+	var H = 31;
+	var total = 0;
+	for (var i = 0; i < string.length; ++i) {
+		total += H * total + string.charCodeAt(i);
+	}
+	total = total % this.table.length;
+	if (total < 0) {
+		total += this.table.length-1;
+	}
+	return parseInt(total);
+}
 	      
-	      
-	                     function put(key, data) {add element 
-	                      var pos = this.betterHash(key);
-			      if (this.table[pos] == undefined) {
-				      this.table[pos] = key;
-				      this.values[pos] = data;
-			      }
-			      else {
-				      while (this.table[pos] != undefined) {
-					      pos++;
-				      }
-				      this.table[pos] = key;
-				      this.values[pos];
-			      }
-			    }
+function put(key, data) {//add element 
+	var pos = this.betterHash(key);
+	if (this.table[pos] == undefined) {
+		this.table[pos] = key;
+		this.values[pos] = data;
+	}
+	else {
+		while (this.table[pos] != undefined) {
+			pos++;
+		}
+		this.table[pos] = key;
+		this.values[pos];
+	}
+}
 
 function get(key) {//retrive element
 	var hash = -1;
@@ -63,15 +62,14 @@ function get(key) {//retrive element
 	return undefined;
 }
 
-	      
-	                       function showDistro() {//display table
-	                        var n = 0;
-	                         for (var i = 0; i < this.table.length; ++i) {
-	                         if (this.table[i] != undefined) {
-	                           print(i + ": " + this.table[i]);
-	                            }
-	                             }
-	                            }
+function showDistro() {//display table
+	var n = 0;
+	for (var i = 0; i < this.table.length; ++i) {
+		if (this.table[i] != undefined) {
+			print(i + ": " + this.table[i]);
+		}
+	}
+}
 
 
 
